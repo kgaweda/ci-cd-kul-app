33fa47bd73dabca369c35c64164aa3931be154a8
kgaweda <kamil.gaweda@outlook.com>
2026-06-01 19:25:18 +0200

    Update home route message to include the current date

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD Demo"
+    return "Hello from Flask CI/CD Demo 01.06.2026"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
